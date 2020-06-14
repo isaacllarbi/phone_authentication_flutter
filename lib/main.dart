9fc2622c9599d93102f2c5f41649b84387bc3c47
Isaac Larbi <isaacllarbi@gmailcom>
2020-06-14 08:22:14 +0000

    ds

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Phone Auth',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.red,
